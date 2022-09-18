1087db4adb19a7840182b4ab6f3abf8e1b81b81e
Eric Whale <hsh048148@gmail.com>
2022-09-19 00:22:54 +0900

    Edit nvim-tree.lua config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -30,29 +30,29 @@ if not status_ok then
 end
 
 -- Have packer use a popup window
--- packer.init {
-  -- display = {
-    -- open_fn = function()
-      -- return require("packer.util").float { border = "rounded" }
-    -- end,
-  -- },
--- }
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
 
 -- Install your plugins here
 return packer.startup(function(use)
 
   -- Packer can manage itself
   use { "wbthomason/packer.nvim", commit="00ec5adef58c5ff9a07f11f45903b9dbbaa1b422" } 
-  -- Useful lua functions used by lots of plugins
   use { "nvim-lua/plenary.nvim", commit="968a4b9afec0c633bc369662e78f8c5db0eba249" }
-  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
-  use { "jiangmiao/auto-pairs", commit="39f06b873a8449af8ff6a3eee716d3da14d63a76" }
-  use { "numToStr/Comment.nvim", commit = "2c26a00f32b190390b664e56e32fd5347613b9e2" }
-  use { "JoosepAlviste/nvim-ts-context-commentstring", commit = "88343753dbe81c227a1c1fd2c8d764afb8d36269" }
-  use { "kyazdani42/nvim-web-devicons", commit = "8d2c5337f0a2d0a17de8e751876eeb192b32310e" }
   
   -- Colorscheme
   use({"NLKNguyen/papercolor-theme"})
+  use { "kyazdani42/nvim-web-devicons", commit = "8d2c5337f0a2d0a17de8e751876eeb192b32310e" }
+  -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    commit = "518e27589c0463af15463c9d675c65e464efc2fe",
+  }
   
   -- cmp (auto-completion)
   use { "hrsh7th/nvim-cmp", commit = "df6734aa018d6feb4d76ba6bda94b1aeac2b378a" } -- The completion plugin
@@ -74,21 +74,23 @@ return packer.startup(function(use)
 
   -- telescope
   use { "nvim-telescope/telescope.nvim", commit = "d96eaa914aab6cfc4adccb34af421bdd496468b0" }
-
-  -- Treesitter
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    commit = "518e27589c0463af15463c9d675c65e464efc2fe",
-  }
-
-  -- Git
-  use { "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" }
-
   -- nvimTree
   use { "kyazdani42/nvim-tree.lua", commit = "bdb6d4a25410da35bbf7ce0dbdaa8d60432bc243" }
 
+
+  -- Functional Plugins
+  -- Git
+  use { "lewis6991/gitsigns.nvim", commit = "c18e016864c92ecf9775abea1baaa161c28082c3" }
+  -- Comment
+  use { "JoosepAlviste/nvim-ts-context-commentstring"}
+  use { "numToStr/Comment.nvim", commit = "2c26a00f32b190390b664e56e32fd5347613b9e2" }
+  -- markdown preview
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+  -- auto pairs
+  use { "jiangmiao/auto-pairs", commit="39f06b873a8449af8ff6a3eee716d3da14d63a76" }
+
+
   -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
     require("packer").sync()
   end
